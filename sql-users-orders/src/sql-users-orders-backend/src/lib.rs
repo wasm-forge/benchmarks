505ff01aa06a21cabc3911bc38739844541f91e8
Stanislav Golubtsov <sgaflv@gmail.com>
2024-11-09 13:23:07 +0100

    benchmark improvements

--- sql-users-orders/src/sql-users-orders-backend/src/lib.rs
+++ sql-users-orders/src/sql-users-orders-backend/src/lib.rs
@@ -18,48 +18,60 @@ thread_local! {
 
 type Result<T = String, E = Error> = std::result::Result<T, E>;
 
-type QueryResult<T = Vec<Vec<String>>, E = Error> = std::result::Result<T, E>;
+type QueryResult<T = Vec<Vec<Option<String>>>, E = Error> = std::result::Result<T, E>;
 
 const MOUNTED_MEMORY_ID: u8 = 20;
 const DB_FILE_NAME: &str = "db.db3";
 
-#[ic_cdk::update]
-fn add(name: String, data: String, age: u32) {
+fn set_pragmas() {
+    // set pragmas
     DB.with(|db| {
         let mut db = db.borrow_mut();
         let db = db.as_mut().unwrap();
-        db.execute(
-            "INSERT INTO person (name, data, age) VALUES (?1, ?2, ?3)",
-            (&name, &data, age),
-        )
-        .unwrap();
+
+        // do not create and destroy the journal file every time, set its size to 0 instead.
+        // This option works faster for normal files,
+        // this option is mandatory, if you are using mounted memory files as a storage.
+        db.pragma_update(None, "journal_mode", &"TRUNCATE" as &dyn ToSql)
+            .unwrap();
+
+        // reduce synchronizations
+        //db.pragma_update(None, "synchronous", &1 as &dyn ToSql)
+        //    .unwrap();
+
+        // use fewer writes to disk with larger memory chunks.
+        // This pragma gives about 10% improvement when adding large batches of new records.
+        // Can slow down up to 30% for database changes scattered accross its memory.
+        // (any small change will cause the DB server to rewrite the whole DB page)
+        //db.pragma_update(None, "page_size", &16384 as &dyn ToSql)
+        //    .unwrap();
+
+        // reduce locks and unlocks, since the canister in the only user of the database with no concurrent connections,
+        // there is no need to lock and unlock the database for each of the queries.
+        // Note: For this mode it is important that you unlock the database before upgrading the canister
+        // by explicitly destroying the connection in the pre_upgrade hook, otherwise the lock file
+        // will be present after upgrade, and you won't be able to open a new connection.
+        db.pragma_update(None, "locking_mode", &"EXCLUSIVE" as &dyn ToSql)
+            .unwrap();
+
+        // temp_store = MEMORY, this disables creating temp files on the disk during complex queries,
+        // this workaround is currently necessary to avoid error when sqlite tries to create a new tmp file
+        db.pragma_update(None, "temp_store", &2 as &dyn ToSql)
+            .unwrap();
+
+        // Add this option to minimize disk reads and work in canister memory instead.
+        // Some operations like batch insertions can have lower performance with this option.
+        // Some operations related to adding indexed records have better performance.
+        //db.pragma_update(None, "cache_size", &1000000 as &dyn ToSql)
+        //    .unwrap();
     });
 }
 
-#[ic_cdk::query]
-fn list() -> Vec<(u64, String, String, u32)> {
-    DB.with(|db| {
-        let mut db = db.borrow_mut();
-        let db = db.as_mut().unwrap();
-        let mut stmt = db
-            .prepare("SELECT id, name, data, age FROM person")
-            .unwrap();
-        let rows = stmt
-            .query_map([], |row| {
-                Ok((
-                    row.get(0).unwrap(),
-                    row.get(1).unwrap(),
-                    row.get(2).unwrap(),
-                    row.get(3).unwrap(),
-                ))
-            })
-            .unwrap();
-        let mut result = vec![];
-        for person in rows {
-            result.push(person.unwrap());
-        }
-        result
-    })
+fn setup_runtime() {
+    init_polyfill();
+    //mount_memory_files();
+    open_database();
+    set_pragmas();
 }
 
 #[ic_cdk::query]
@@ -70,22 +82,24 @@ fn query(sql: String) -> QueryResult {
 
         let mut stmt = db.prepare(&sql).unwrap();
         let cnt = stmt.column_count();
+
         let mut rows = stmt.query([]).unwrap();
-        let mut res: Vec<Vec<String>> = Vec::new();
+
+        let mut res: Vec<Vec<Option<String>>> = Vec::new();
 
         loop {
             match rows.next() {
                 Ok(row) => match row {
                     Some(row) => {
-                        let mut vec: Vec<String> = Vec::new();
+                        let mut vec: Vec<Option<String>> = Vec::new();
                         for idx in 0..cnt {
                             let v = row.get_ref_unwrap(idx);
                             match v.data_type() {
-                                Type::Null => vec.push(String::from("")),
-                                Type::Integer => vec.push(v.as_i64().unwrap().to_string()),
-                                Type::Real => vec.push(v.as_f64().unwrap().to_string()),
-                                Type::Text => vec.push(v.as_str().unwrap().parse().unwrap()),
-                                Type::Blob => vec.push(hex::encode(v.as_blob().unwrap())),
+                                Type::Null => vec.push(None),
+                                Type::Integer => vec.push(Some(v.as_i64().unwrap().to_string())),
+                                Type::Real => vec.push(Some(v.as_f64().unwrap().to_string())),
+                                Type::Text => vec.push(Some(v.as_str().unwrap().parse().unwrap())),
+                                Type::Blob => vec.push(Some(hex::encode(v.as_blob().unwrap()))),
                             }
                         }
                         res.push(vec)
@@ -111,10 +125,16 @@ fn execute(sql: &str) {
     });
 }
 
-fn mount_memory_files() {
+fn init_polyfill() {
     MEMORY_MANAGER.with(|m| {
         let m = m.borrow();
         ic_wasi_polyfill::init_with_memory_manager(&[0u8; 32], &[], &m, 200..210);
+    });
+}
+
+fn mount_memory_files() {
+    MEMORY_MANAGER.with(|m| {
+        let m = m.borrow();
 
         // mount virtual memory as file for faster DB operations
         let memory = m.get(MemoryId::new(MOUNTED_MEMORY_ID));
@@ -166,49 +186,9 @@ fn create_indices() {
     execute("CREATE INDEX IF NOT EXISTS idx_orders_user_id ON orders(user_id);");
 }
 
-fn set_pragmas() {
-    // set pragmas
-    DB.with(|db| {
-        let mut db = db.borrow_mut();
-        let db = db.as_mut().unwrap();
-
-        // do not create and destroy the journal file every time, set its size to 0 instead.
-        // This option works faster for normal files,
-        // this option is also mandatory, if you are using mounted memory files as a storage.
-        db.pragma_update(None, "journal_mode", &"TRUNCATE" as &dyn ToSql)
-            .unwrap();
-
-        // use fewer writes to disk with larger memory chunks.
-        // This pragma gives about 10% improvement when adding large batches of new records.
-        // Can slow down up to 30% for operations that change the database in different places.
-        // (any small change will cause the DB server to rewrite the whole DB page)
-        //db.pragma_update(None, "page_size", &16384 as &dyn ToSql)
-        //    .unwrap();
-
-        // reduce locks and unlocks, since the canister in the only user of the database with no concurrent connections,
-        // there is no need to lock and unlock the database for each of the queries.
-        db.pragma_update(None, "locking_mode", &"EXCLUSIVE" as &dyn ToSql)
-            .unwrap();
-
-        // temp_store = MEMORY, disables creating temp files during complex queries, improves performance,
-        // this workaround is currently necessary to avoid error when creating a new tmp file
-        db.pragma_update(None, "temp_store", &2 as &dyn ToSql)
-            .unwrap();
-
-        // Add this option to minimize disk reads and work in canister memory instead.
-        // Some operations, like batch insertions can have lower performance with this option.
-        // Some operations related to adding indexed records have better performance.
-        db.pragma_update(None, "cache_size", &1000000 as &dyn ToSql)
-            .unwrap();
-    });
-}
-
 #[ic_cdk::init]
 fn init() {
-    mount_memory_files();
-
-    open_database();
-    set_pragmas();
+    setup_runtime();
     create_tables();
 }
 
@@ -219,10 +199,7 @@ fn pre_upgrade() {
 
 #[ic_cdk::post_upgrade]
 fn post_upgrade() {
-    mount_memory_files();
-
-    open_database();
-    set_pragmas();
+    setup_runtime();
 }
 
 #[derive(CandidType, Deserialize, Debug)]
@@ -281,13 +258,13 @@ fn add_orders(offset: usize, count: usize, id_mod: usize) -> Result {
                 let id = (offset + i + 1) * 13 % id_mod + 1;
 
                 stmt.execute(rusqlite::params![id, (id * 100 + id * 17) / 15])
-                    .expect("INSERT USER FAILED!");
+                    .expect("INSERT ORDER FAILED!");
 
                 i += 1;
             }
         }
 
-        tx.commit().expect("COMMIT USER INSERTION FAILED!");
+        tx.commit().expect("COMMIT ORDER INSERTION FAILED!");
 
         Ok("add_orders OK".to_string())
     })
@@ -347,6 +324,24 @@ mod benches {
     }
 
     #[bench(raw)]
+    fn bench_select_like_on_indexed_field() -> BenchResult {
+        let user_count = COUNT / 10;
+        add_users(0, user_count).unwrap();
+        add_orders(0, COUNT, user_count).unwrap();
+        create_indices();
+
+        bench_fn(|| {
+            query(
+                r#"
+                SELECT * FROM users WHERE email LIKE 'user%';
+                "#
+                .to_string(),
+            )
+            .unwrap();
+        })
+    }
+
+    #[bench(raw)]
     fn bench_add_100_indexed_orders() -> BenchResult {
         let user_count = COUNT / 10;
         add_users(0, user_count).unwrap();
@@ -380,6 +375,58 @@ mod benches {
 
         bench_fn(|| {
             add_orders(0, COUNT, user_count).unwrap();
+        })
+    }
+
+    #[bench(raw)]
+    fn bench_add_remove_durability() -> BenchResult {
+        let user_count = COUNT / 10;
+        add_users(0, user_count).unwrap();
+        add_orders(0, COUNT, user_count).unwrap();
+        create_indices();
+
+        bench_fn(|| {
+            for i in 0..100 {
+                let idx1 = i * 100;
+                let idx2 = (i + 1) * 100;
+                execute(&format!(
+                    "DELETE FROM orders WHERE user_id > {idx1} AND user_id <= {idx2} "
+                ));
+                add_orders(0, 1000, user_count).unwrap();
+            }
+        })
+    }
+
+    #[bench(raw)]
+    fn bench_transaction_rollback() -> BenchResult {
+        let user_count = COUNT / 10;
+        add_users(0, user_count).unwrap();
+        add_orders(0, COUNT, user_count).unwrap();
+        create_indices();
+
+        bench_fn(|| {
+            let res = query("SELECT COUNT(*) FROM orders".to_string()).unwrap();
+            let s = res[0][0].clone().unwrap();
+            let cnt: i64 = s.parse().expect("Not a valid number");
+
+            assert_eq!(cnt, 1000000);
+
+            execute("BEGIN TRANSACTION");
+            execute("DELETE FROM orders WHERE order_id > 900000");
+
+            let res = query("SELECT COUNT(*) FROM orders".to_string()).unwrap();
+            let s = res[0][0].clone().unwrap();
+            let cnt: i64 = s.parse().expect("Not a valid number");
+
+            assert_eq!(cnt, 900000);
+
+            execute("ROLLBACK");
+
+            let res = query("SELECT COUNT(*) FROM orders".to_string()).unwrap();
+            let s = res[0][0].clone().unwrap();
+            let cnt: i64 = s.parse().expect("Not a valid number");
+
+            assert_eq!(cnt, 1000000);
         })
     }
 }
