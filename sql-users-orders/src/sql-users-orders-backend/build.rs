8a9d2cb14ed9010ff5dbebc03c195a7bb0c61f68
wasm-forge <122647775+wasm-forge@users.noreply.github.com>
2025-06-11 09:13:17 +0200

    clean-up, version update

--- sql-users-orders/src/sql-users-orders-backend/build.rs
+++ sql-users-orders/src/sql-users-orders-backend/build.rs
@@ -1,18 +1,16 @@
 use glob::glob;
-use std::env;
 use std::path::PathBuf;
 
 fn main() {
-
     // locate wasi builtins
-    let sdk_path = if let Ok(sdk_path) = env::var("WASI_SDK") {
+    let sdk_path = if let Ok(sdk_path) = std::env::var("WASI_SDK") {
         sdk_path
     } else {
         "/opt/wasi-sdk".to_string()
     };
 
     let pattern = format!("{sdk_path}/lib/clang/*/lib/wasip1");
-    
+
     let paths: Vec<PathBuf> = glob(&pattern)
         .expect("Failed to read glob pattern")
         .filter_map(Result::ok)
@@ -26,5 +24,4 @@ fn main() {
     } else {
         panic!("Could not find clang wasm32 builtins under '{}'", pattern);
     }
-
 }
