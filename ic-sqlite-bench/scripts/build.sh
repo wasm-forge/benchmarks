e56e9f968f5f27b25f412ef70029e161da163582
wasm-forge <122647775+wasm-forge@users.noreply.github.com>
2025-08-07 00:03:32 +0200

    migrate ic-sqlite benchmark to ic-rusqlite

--- ic-sqlite-bench/scripts/build.sh
+++ ic-sqlite-bench/scripts/build.sh
@@ -1,37 +1,15 @@
 #!/bin/bash
+
 dfx canister create --all
 
-export WASI_TARGET=wasm32-wasip1
-export WASI_TARGET_=`echo $WASI_TARGET | tr '-' '_'`
-
-export CC_$WASI_TARGET_="/opt/wasi-sdk/bin/clang"
-export CFLAGS_$WASI_TARGET_="--sysroot=/opt/wasi-sdk/share/wasi-sysroot"
-
-export RELEASE_DIR=target/$WASI_TARGET/release
+export RELEASE_DIR=target/wasm32-wasip1/release
 
 rm -f $RELEASE_DIR/*.wasm.gz $RELEASE_DIR/*.wasm 
 
 set -e
 
-pushd `pwd`
+cargo build --release --target wasm32-wasip1
 
-if [ "$(basename "$PWD")" = "scripts" ]; then
-  cd ..
-fi
-
-export PROJECT_NAME="$(basename "$PWD")"
-export PROJECT_NAME_=`echo $PROJECT_NAME | tr '-' '_'`
-
-cargo build --release --target $WASI_TARGET
-
-mv $RELEASE_DIR/"$PROJECT_NAME_"_backend.wasm $RELEASE_DIR/built.wasm
-
-ic-wasm $RELEASE_DIR/built.wasm -o $RELEASE_DIR/meta.wasm metadata candid:service -f ./src/"$PROJECT_NAME"-backend/"$PROJECT_NAME"-backend.did -v public
-
-wasi2ic $RELEASE_DIR/meta.wasm $RELEASE_DIR/no_wasi.wasm
-
-cp $RELEASE_DIR/no_wasi.wasm $RELEASE_DIR/"$PROJECT_NAME_"_backend.wasm
+wasi2ic $RELEASE_DIR/ic_sqlite_bench_backend.wasm $RELEASE_DIR/no_wasi.wasm
 
 gzip -f $RELEASE_DIR/no_wasi.wasm > $RELEASE_DIR/no_wasi.wasm.gz
-
-popd
--- ic-sqlite-bench/src/ic-sqlite-bench-backend/build.rs
+++ ic-sqlite-bench/src/ic-sqlite-bench-backend/build.rs
@@ -3,7 +3,6 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-
     // locate wasi builtins
     let sdk_path = if let Ok(sdk_path) = env::var("WASI_SDK") {
         sdk_path
@@ -11,20 +10,37 @@ fn main() {
         "/opt/wasi-sdk".to_string()
     };
 
-    let pattern = format!("{sdk_path}/lib/clang/*/lib/wasip1");
-    
+    let sysroot = format!("{sdk_path}/share/wasi-sysroot");
+    println!("cargo:rustc-env=WASI_SYSROOT={sysroot}",);
+
+    let pattern = format!("{sdk_path}/lib/clang/*/lib/*wasip1");
+
     let paths: Vec<PathBuf> = glob(&pattern)
         .expect("Failed to read glob pattern")
         .filter_map(Result::ok)
         .collect();
 
     if let Some(path) = paths.last() {
-        // use the latest version we have found
-
+        // use the latest version that we have found
         println!("cargo:rustc-link-search={}", path.display());
-        println!("cargo:rustc-link-arg=-lclang_rt.builtins-wasm32");
-    } else {
-        panic!("Could not find clang wasm32 builtins under '{}'", pattern);
-    }
 
+        let builtins: Vec<PathBuf> = glob(&format!("{}/*", path.display()))
+            .expect("Failed to read glob pattern")
+            .filter_map(Result::ok)
+            .collect();
+
+        if let Some(b) = builtins.first() {
+            let name = b
+                .file_stem()
+                .expect("builtins not found")
+                .to_string_lossy()
+                .to_string();
+
+            println!("cargo:rustc-link-arg=-l{}", &name[3..]);
+        } else {
+            panic!("Could not find clang wasm32 builtins under '{pattern}'");
+        }
+    } else {
+        panic!("Could not find clang wasm32 builtins under '{pattern}'");
+    }
 }
